0192e285f2f1202f4256a9632adcf142de357f66
vdeepak <vivek.deepak@extentor.com>
2016-04-19 16:04:45 -0400

    first Commuit

--- server.js
+++ server.js
@@ -18,3 +18,16 @@ app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+var pg = require('pg');
+
+var connectionString = "postgres://lfzauzjimnjgkc:YIAibDcidnwKxHiWDDK8p1qvRd@ec2-54-163-247-73.compute-1.amazonaws.com:5432/d7054ihui87aec"
+
+pg.connect(connectionString, function(err, client, done) {
+  client.query('SELECT * FROM salesforcedevorg.position__c', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+  });
+});
+
